ab48a1e3379b74eca36a583a34cff0b3d45efd38
Chen Jin <chenjin@kingsoft.com>
2016-08-01 15:25:08 +0800

    补充测试用例提高覆盖率

--- test/adp.js
+++ test/adp.js
@@ -4,6 +4,10 @@ var should = require('should');
 require('should-http');
 var path = require('path');
 var fs = require('fs');
+var urllib = require('urllib');
+var config = require('../config');
+var xml2json = require("../lib/util").xml2json;
+var debug = require('debug')('adptest');
 
 var ak = process.env.AK || 'WRHLHOZQD3OY3VTROMGQ';
 var sk = process.env.SK || 'dWSmyMmVcpahaZphUdyVz11myMIoCAsOKeZ6wi4T';
@@ -89,6 +93,63 @@ describe('Asynchronous Data Processing', function () {
         });
     });
 
+
+    ////视频切片
+    it('avm3u8 : convert a video to HTTP Live Streaming format', function (done) {
+        var client = new KS3(ak, sk, bucketName,'HANGZHOU');
+        var key = 'a.mp4';
+        var resultVideoName = 'video/a.m3u8';
+        this.timeout(30000);
+        client.object.put({
+                Bucket: bucketName,
+                Key: key,
+                subResource:'adp',
+                isNoContent: true
+            },
+            function (err, data, res) {
+                should.not.exist(err);
+                res.should.have.status(200);
+
+                var taskid = res.headers.taskid;
+
+                //等8s转换时间
+                setTimeout(getAVInfo, 8000);
+
+                //通过queryadp获取生成的文件的元数据验证处理是否成功
+                function getAVInfo() {
+                    var uri = config.protocol + '://' + config.baseUrl + '/' + taskid + '?queryadp&tag=avinfo';
+                    var headers = {
+                      'User-Agent': config.ua
+                    };
+                    var data = {
+                        headers: headers,
+                        method: 'GET',
+                        timeout: 1000 * 60 * 60
+                    }
+                    urllib.request(uri, data, function(err, result, res) {
+                        should.not.exist(err);
+                        res.should.have.status(200);
+                        var data = '';
+                        if ( !!result) {
+                            var dataStr = result.toString();
+                            data = dataStr;
+                            dataStr = dataStr.replace(/\s\w+\:\w+=\"\S+\"/g, '');
+                            var json = xml2json.parser(dataStr,'notifystatus,notifydesc', 'html');
+                            data = json;
+                            debug('res:' + JSON.stringify(data));
+                            data.indexOf('task is process success').should.greaterThan(-1);
+                            data.indexOf('&lt;![CDATA[success]]&gt;').should.greaterThan(-1);
+                            done();
+                        }
+                    });
+                }
+
+            },
+            {
+                'kss-async-process': 'tag=avm3u8&segtime=2|tag=saveas&bucket=' + bucketName + '&object=' + new Buffer(resultVideoName).toString('base64'),
+                'kss-notifyurl': 'http://10.4.2.38:19090/'
+            });
+    });
 
     after(function () {
 
--- test/upload.js
+++ test/upload.js
@@ -9,7 +9,7 @@ var sk = process.env.SK || 'dWSmyMmVcpahaZphUdyVz11myMIoCAsOKeZ6wi4T';
 var bucketName = process.env.BUCKET || 'ks3-sdk-test';
 var bigFile = process.env.BIGFILE || path.join(__dirname, './assets/风之万里,黎明之空.txt');
 var updir = process.env.UPDIR || path.join(__dirname, './assets/updir/');
-
+var debug = require('debug')('test-upload');
 
 
 describe('upload a file', function() {
@@ -56,6 +56,43 @@ describe('upload a file', function() {
 	});
 });
 
+describe('test multipart upload', function() {
+	var tmpFilePath = path.join(__dirname, './assets/updir/bigFileOver100M.tmp')
+	before(function() {
+		/**
+		 * 创建一个大于100M的文件
+		 */
+		var mock_data = '金山云CDN服务将用户内容分发到全球700余个边缘节点，提高用户访问网站的响应速度与网站的可用性，通过智能监控实时调度异常节点资源，保证全局性能和可用性, 控制台可以帮助用户完成新增CDN加速域名、刷新/预加载缓存目录、URL等配置任务，并提供数据分析等统计图表。本文档主要介绍CDN控制台操作手册。';
+		mock_data = new Buffer(new Array(20).join(mock_data));
+		var unit_size = mock_data.length;
+		var total = 101* 1024* 1024; //101 MB
+		var sumSize = 0;
+		while(sumSize < total) {
+			fs.appendFileSync(tmpFilePath, mock_data);
+			sumSize += unit_size;
+			debug('sumSize: ' + sumSize);
+		}
+	});
+	it('upload a big file by multipart upload', function(done) {
+		this.timeout(300000);
+		var client = new KS3(ak, sk, bucketName,'HANGZHOU');
+		var key = 'multipartUpload/bigFile.tmp';
+		client.upload.start({
+				Bucket: bucketName,
+				filePath: tmpFilePath,
+				Key: key
+			},
+			function(err, data, res) {
+				should.not.exist(err);
+				res.should.have.status(200);
+				done();
+			});
+	});
+	after(function() {
+		//删除临时文件
+		fs.unlinkSync(tmpFilePath);
+	});
+});
 
 
 describe('upload a directory', function() {
